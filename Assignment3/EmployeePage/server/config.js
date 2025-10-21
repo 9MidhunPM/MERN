fd9d59b33e665c4a87f1998e8f7a54486e760e49
9MidhunPM <midhun.titan@gmail.com>
2025-10-21 21:29:26 +0530

    latest

--- Assignment3/EmployeePage/server/config.js
+++
@@ -1,8 +0,0 @@
-// Configuration for data source
-module.exports = {
-  // Set to 'custom' to use local sample data, 'external' to use jsonplaceholder API
-  dataSource: 'custom', // Options: 'custom' or 'external'
-  
-  // External API URL (used when dataSource is 'external')
-  externalApiUrl: 'https://jsonplaceholder.typicode.com/users'
-};
